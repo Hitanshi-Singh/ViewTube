2bb49ddeb18a945568e14c8049e74a937a4dc697
Hitanshi-Singh <tech.hitanshi@gmail.com>
2024-08-18 21:42:52 +0530

    correction

---
+++ src/assets/utilities/constants.jsx
@@ -0,0 +1,2 @@
+
+export const YOUTUBE_VIDEOS_API=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${import.meta.env.VITE_API_KEY}`
